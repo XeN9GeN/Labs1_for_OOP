f2f8ef59a1326789b7c12ed694a44afbf013dacb
XeN9GeN <k.vanin@g.nsu.ru>
2025-09-16 01:22:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 #include <cstdbool>
 using namespace std;
 
-bool comparesion_for_null(int a, int b) {
+bool comparesion_for_nulls(int a, int b) {
 	return a > b;
+}
+
+bool comparesion_for_vector(vector <int>& num1, vector <int>& num2) {
+	if (num1.size() != num2.size()) {
+		return num1.size() >= num2.size();
+	}
+
+	int s = num1.size();
+	for (int i = 0; i < s; i++) {
+		if (num1[i] != num2[i]) {
+			return num1[i] > num2[i];
+		}
+	}
+	return false;
 }
 
 
@@ -25,66 +39,86 @@ vector<int> slozhenie(vector<int>& first_num, vector<int>& second_num) {
 	return res;
 }
 
-bool comparesion_for_vector(vector <int>&num1,)
-
-
-
-
-
-
-
-
-int minus(vector<int>& first_num, vector<int>& second_num) {
+vector<int> minuselo(vector<int>& num1, vector<int>& num2) {
+	vector<int> res;
+	bool flag = false;
 	
+	if (!comparesion_for_vector(num1, num2)) {
+		swap(num1, num2);
+		flag = true;
+	}
+
+	int cf = 0;
+	int s = num1.size();
+	for (int i = s - 1; i >= 0; i--) {
+		int diff = num1[i] - num2[i] - cf;
+		if (diff <  0) {
+			diff += 10; 
+			cf = 1;
+		}
+		else
+			cf = 0;
+		res.insert(res.begin(), diff);
+		
+	}
+	if (flag) {
+		res.insert(res.begin(), -1);//минус
+	}
+
+	return res;
 }
+
+
 
 int main() {
 	int n, m;
-	vector<int>first_num, second_num;
+	vector<int>num1, num2;
 	
 	cout << "First num: ";
 	cin >> n;
 	cout << "Second num: ";
 	cin >> m;
 
-	int c1=0, c2=0;
+	int size1=0, size2=0;
 	int temp1 = n, temp2 = m;
 
 	while (temp1 != 0) {
-		first_num.insert(first_num.begin(), temp1 % 10);
-		c1++;
+		num1.insert(num1.begin(), temp1 % 10);
+		size1++;
 		temp1 /= 10;
 	}
 	while (temp2 != 0) {
-		second_num.insert(second_num.begin(), temp2 % 10);
-		c2++;
+		num2.insert(num2.begin(), temp2 % 10);
+		size2++;
 		temp2 /= 10;
 	}
 
-	int r = abs(c1 - c2);
-	if (comparesion_for_null(c1,c2)) {
-		do {
-			second_num.insert(second_num.begin(),0);
+	int r = abs(size1 - size2);
+	if (comparesion_for_nulls(size1, size2)) {
+		while (r > 0) {
+			num2.insert(num2.begin(), 0);
 			r--;
-		} while (r > 0);
+		}
 	}
 	else {
-		do {
-			first_num.insert(first_num.begin(), 0);
+		while (r > 0) {
+			num1.insert(num1.begin(), 0);
 			r--;
-		} while (r > 0);
+		}
 	}
 
-
-
-
-
-	for (int i : first_num) {
-		cout << i << " ";
+	vector<int> res = minuselo(num1, num2);
+	cout << "Answer: ";
+	if (res[0] == -1) {
+		cout << "-";
+		for (int i = 1; i < res.size(); i++) {
+			cout << res[i];
+		}
 	}
-	cout << endl;
-	for (int i : second_num) {
-		cout << i << " ";
+	else {
+		for (int i : res) {
+			cout << i;
+		}
 	}
 	return 0;
 }
